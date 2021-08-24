0dfd217f24701319c3be609b695f88c017a4c9af
Rafael Fayan <rafaelfayan@gmail.com>
2021-08-24 16:43:47 -0300

    Added error_handling and method description to get_discount gRPC method
    Signed-off-by: Rafael Fayan <rafaelfayan@gmail.com>

--- hash_retail/core/grpc/discount.py
+++ hash_retail/core/grpc/discount.py
@@ -9,9 +9,15 @@ GRPC_SERVER_ADDRESS = f"{environ['GRPC_HOST']}:{environ['GRPC_PORT']}"
 
 
 async def get_discount_percentage(product_id: int) -> float:
-    async with grpc.aio.insecure_channel(GRPC_SERVER_ADDRESS) as channel:
-        stub = discount_pb2_grpc.DiscountStub(channel)
-        response = await stub.GetDiscount(discount_pb2.GetDiscountRequest(productID=product_id))
+    """Get product discount from gRPC service and return its percentage as a float value."""
+    try:
+        async with grpc.aio.insecure_channel(GRPC_SERVER_ADDRESS) as channel:
+            stub = discount_pb2_grpc.DiscountStub(channel)
+            response = await stub.GetDiscount(discount_pb2.GetDiscountRequest(productID=product_id))
+    except (grpc.aio.BaseError, grpc.RpcError) as grpc_error:
+        # If gRPC Service is not available, log the error message and return 0% discount
+        logging.warning("gRPC error: %s", grpc_error)
+        return 0
 
     discount_percentage = round(response.percentage, 2)
     logging.debug("Discount percentage: %s", discount_percentage)
